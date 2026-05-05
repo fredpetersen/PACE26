cb02e70339409228242c81047d227e0a1ab5a802
Frederik Petersen <fred.p@live.dk>
2026-05-05 13:15:59 +0200

    Fix main to use new naming

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 #include <forest.h>
-#include <two_tree_test_solver.h>
-#include <two_tree_solver.h>
+#include <test_solver.h>
+#include <solver.h>
 #include <input_reader.h>
 
 // #include "input_reader.cpp"
@@ -16,13 +16,13 @@ int main() {
     // std::cout << "# Input Parsed" << std::endl;
 
     // std::cout << "# Creating Solver" << std::endl;
-    TwoTreeSolver solver(problemInstance.forests, problemInstance.leafCount);
-    // TwoTreeTestSolver solver(problemInstance.forests, problemInstance.leafCount);
+    Solver solver(problemInstance.forests, problemInstance.leafCount);
+    // TestSolver solver(problemInstance.forests, problemInstance.leafCount);
 
     // std::cout << "# Solving" << std::endl;
     auto result = solver.solve();
     // int result = solver.test();
-    
+
     // std::cout << "# Solution:" << std::endl;
     result->printForestNewick();
     return 0;
